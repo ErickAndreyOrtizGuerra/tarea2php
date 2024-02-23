958b8caf08d665bd6b686e47b041525ee74ccdaa
erickortiz <guerraerick54@gmail.com>
2024-02-22 21:16:11 -0600

    mayor y menor

--- ejercicios.php
+++ ejercicios.php
@@ -1,20 +1,15 @@
 <?php
-// Fecha de nacimiento de la persona (en formato de texto)
-$fecha_nacimiento = "2004-05-15";
+// Definir los dos numeros
+$primerNumero = 26;
+$segundoNumero = 56;
 
-// Convertir la fecha de nacimiento 
-$timestamp_nacimiento = strtotime($fecha_nacimiento);
-
-// Obtener la fecha actual 
-$timestamp_actual = time();
-
-// Calcular la diferencia en segundos entre la fecha actual y la fecha de nacimiento
-$diferencia = $timestamp_actual - $timestamp_nacimiento;
-
-// Convertir la diferencia de segundos a years
-$edad = floor($diferencia / (60 * 60 * 24 * 365.25));
-
-// Mostrar la edad
-echo " tienes $edad años.";
+// Verificar cual es mayor usando una estructura condicional if-else
+if ($primerNumero > $segundoNumero) {
+    echo "El primer numero ($primerNumero) es mayor que el segundo numero ($segundoNumero).";
+} elseif ($primerNumero < $segundoNumero) {
+    echo "El segundo numero ($segundoNumero) es mayor que el primer numero ($primerNumero).";
+} else {
+    echo "Ambos numeros son iguales.";
+}
 
 ?>
