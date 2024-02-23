10e3d3a4acaf0777540d8af393060a13d024afca
erickortiz <guerraerick54@gmail.com>
2024-02-22 21:17:53 -0600

    par o impar

--- ejercicios.php
+++ ejercicios.php
@@ -1,15 +1,11 @@
 <?php
-// Definir los dos numeros
-$primerNumero = 26;
-$segundoNumero = 56;
+// Definir el numero a verificar
+$numeroSeleccionado = 36; // Puedes cambiar este numero al que desees verificar
 
-// Verificar cual es mayor usando una estructura condicional if-else
-if ($primerNumero > $segundoNumero) {
-    echo "El primer numero ($primerNumero) es mayor que el segundo numero ($segundoNumero).";
-} elseif ($primerNumero < $segundoNumero) {
-    echo "El segundo numero ($segundoNumero) es mayor que el primer numero ($primerNumero).";
+// Verificar si el numero es par o impar usando una estructura condicional if-else
+if ($numeroSeleccionado % 2 == 0) {
+    echo "El numero seleccionado ($numeroSeleccionado) es par.";
 } else {
-    echo "Ambos numeros son iguales.";
+    echo "El numero seleccionado ($numeroSeleccionado) es impar.";
 }
-
 ?>
