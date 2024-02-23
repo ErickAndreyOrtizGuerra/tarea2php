15351613741bb73e71872eaee9a75062a2b96707
erickortiz <guerraerick54@gmail.com>
2024-02-22 21:33:44 -0600

    array alumnos

--- ejercicios.php
+++ ejercicios.php
@@ -1,10 +1,21 @@
 <?php
-// Generar un array con 20 numeros aleatorios entre 1 y 100
-$numeros = array_map(fn() => rand(1, 10), range(1, 20));
+// Definir un array asociativo con los datos de los alumnos
+$alumnos = array(
+    array('alumno' => 'Erick', 'curso' => 'Matematicas', 'nota' => 85),
+    array('alumno' => 'Leo', 'curso' => 'Geografia', 'nota' => 30),
+    array('alumno' => 'Cristian', 'curso' => 'Musica', 'nota' => 45),
+    array('alumno' => 'Andrea', 'curso' => 'Matematicas', 'nota' => 88),
+    array('alumno' => 'Sofia', 'curso' => 'Historia', 'nota' => 70),
+    array('alumno' => 'Laura', 'curso' => 'Naturales', 'nota' => 92),
+    array('alumno' => 'Carlos', 'curso' => 'Quimica', 'nota' => 82),
+    array('alumno' => 'Andres', 'curso' => 'Historia', 'nota' => 60),
+    array('alumno' => 'Nicolas', 'curso' => 'Sociales', 'nota' => 95),
+    array('alumno' => 'Julia', 'curso' => 'Quimica', 'nota' => 100)
+);
 
-// Ordenar los numeros de mayor a menor
-rsort($numeros);
-
-// Mostrar los numeros aleatorios generados y ordenados
-echo "Los numeros aleatorios generados y ordenados de mayor a menor: " . implode(' ', $numeros);
+// Recorrer el array para mostrar los datos de los alumnos
+echo "Datos de los alumnos:";
+foreach ($alumnos as $alumno) {
+    echo "Alumno: " . $alumno['alumno'] . ", Curso: " . $alumno['curso'] . ", Nota: " . $alumno['nota'] . "";
+}
 ?>
